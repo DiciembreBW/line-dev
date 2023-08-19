b201631ff68486be3c40d6392b00b4cd51e18ef7
mac <mac@macs-Mac-mini.local>
2023-08-19 22:03:45 +0700

    3d orbit

--- app/3d/page.tsx
+++ app/3d/page.tsx
@@ -1,40 +1,55 @@
 "use client";
 import React, {useState} from "react";
 import {Canvas} from "@react-three/fiber";
-import {Environment, ModelSample, Shadow} from "@/libs/components/Model";
-import {CameraControls, PresentationControls} from "@react-three/drei";
+import {
+	Environment,
+	ModelSample,
+	Shadow,
+	StickyControl,
+} from "@/libs/components/Model";
+import {
+	CameraControls,
+	Grid,
+	OrbitControls,
+	PresentationControls,
+	TransformControls,
+} from "@react-three/drei";
 
 type Props = {};
 // https://codesandbox.io/s/qyz5r?file=/src/App.js
 
 export default function page({}: Props) {
 	return (
-		<div className="h-screen">
+		<div className="h-screen bg-slate-700">
 			<Canvas shadows camera={{position: [0, 0, 10], fov: 25}}>
-				<PresentationControls
-					// config={{mass: 2, tension: 500}}
-					// snap={{mass: 4, tension: 1500}}
-					// rotation={[0, 0.3, 0]}
-
-					config={{mass: 2, tension: 500}}
-					snap={{mass: 4, tension: 1500}}
-					rotation={[0, 0.3, 0]}
-					polar={[-Math.PI / 3, Math.PI / 3]}
-					azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
-					<ModelSample />
-				</PresentationControls>
-
 				<Environment />
 				<Shadow />
+
 				{/* <CameraControls makeDefault /> */}
-				<ambientLight intensity={0.5} />
-				<spotLight
+				{/* <OrbitControls /> */}
+				{/* <Grid /> */}
+				{/* <TransformControls mode="translate"> */}
+
+				<ModelSample />
+				<OrbitControls
+					// maxDistance={12}
+					rotateSpeed={1.25}
+					dampingFactor={0.2}
+					// autoRotate
+					// minAzimuthAngle={Math.PI / 4}
+					// maxAzimuthAngle={Math.PI / 2}
+					minPolarAngle={Math.PI / 2.5}
+					maxPolarAngle={Math.PI / 2}
+				/>
+				{/* </TransformControls> */}
+				{/* <ambientLight intensity={0.5} /> */}
+				{/* <spotLight
 					position={[10, 10, 10]}
 					angle={0.15}
 					penumbra={1}
 					shadow-mapSize={2048}
 					castShadow
-				/>
+				/> */}
 			</Canvas>
 		</div>
 	);
--- libs/components/Model.tsx
+++ libs/components/Model.tsx
@@ -1,4 +1,10 @@
-import {ContactShadows, EnvironmentCube} from "@react-three/drei";
+import {
+	ContactShadows,
+	EnvironmentCube,
+	PresentationControls,
+} from "@react-three/drei";
+import {group} from "console";
+import React from "react";
 
 export function Environment() {
 	return <EnvironmentCube preset="city" />;
@@ -7,7 +13,7 @@ export function Environment() {
 export function Shadow() {
 	return (
 		<ContactShadows
-			position={[0, -1.4, 0]}
+			position={[0, -1.2, 0]}
 			opacity={0.75}
 			scale={10}
 			blur={2.5}
@@ -16,11 +22,27 @@ export function Shadow() {
 	);
 }
 
-export function ModelSample() {
+export function ModelSample(props: any) {
 	return (
-		<mesh>
-			<boxGeometry />
-			<meshBasicMaterial />
-		</mesh>
+		<group>
+			<mesh>
+				<boxGeometry />
+				{/* <meshBasicMaterial /> */}
+				{/* <wireframeGeometry /> */}
+			</mesh>
+		</group>
+	);
+}
+
+export function StickyControl({children}: {children: React.ReactNode}) {
+	return (
+		<PresentationControls
+			config={{mass: 2, tension: 500}}
+			snap={{mass: 4, tension: 1500}}
+			rotation={[0, 0.3, 0]}
+			polar={[-Math.PI / 3, Math.PI / 3]}
+			azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
+			{children}
+		</PresentationControls>
 	);
 }
