6d120621b1ad6952a463cdd40ed3d404be76b9da
mac <mac@macs-Mac-mini.local>
2023-08-20 20:41:03 +0700

    add podium model

--- app/3d/page.tsx
+++ app/3d/page.tsx
@@ -12,25 +12,28 @@ import {
 	Grid,
 	OrbitControls,
 	PresentationControls,
+	Stage,
 	TransformControls,
 } from "@react-three/drei";
 
+import {Model} from "@/libs/components/my_models/Po";
+
 type Props = {};
 // https://codesandbox.io/s/qyz5r?file=/src/App.js
+// https://www.youtube.com/watch?v=Nxd9L6X8quo
 
 export default function page({}: Props) {
 	return (
 		<div className="h-screen bg-slate-700">
-			<Canvas shadows camera={{position: [0, 0, 10], fov: 25}}>
-				<Environment />
-				<Shadow />
+			<Canvas shadows camera={{position: [0, 0, 15], fov: 35}}>
+				{/* <Environment /> */}
+				{/* <Shadow /> */}
 
 				{/* <CameraControls makeDefault /> */}
 				{/* <OrbitControls /> */}
 				{/* <Grid /> */}
 				{/* <TransformControls mode="translate"> */}
 
-				<ModelSample />
 				<OrbitControls
 					// maxDistance={12}
 					rotateSpeed={1.25}
@@ -40,6 +43,8 @@ export default function page({}: Props) {
 					// maxAzimuthAngle={Math.PI / 2}
 					minPolarAngle={Math.PI / 2.5}
 					maxPolarAngle={Math.PI / 2}
+					autoRotate
+					autoRotateSpeed={0.4}
 				/>
 				{/* </TransformControls> */}
 				{/* <ambientLight intensity={0.5} /> */}
@@ -50,6 +55,16 @@ export default function page({}: Props) {
 					shadow-mapSize={2048}
 					castShadow
 				/> */}
+
+				{/* models */}
+				{/* <ModelSample /> */}
+
+				<Stage
+					adjustCamera={2}
+					intensity={1.5}
+					shadows={{type: "contact", colorBlend: 1, opacity: 0.5}}>
+					<Model />
+				</Stage>
 			</Canvas>
 		</div>
 	);
