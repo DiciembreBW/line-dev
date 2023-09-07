3f699ab6998b156e5ccb8a8c3568d663ea60ee6b
mac <mac@macs-Mac-mini.local>
2023-09-07 21:35:21 +0700

    fix-4

--- app/workspace/[id]/page.tsx
+++ app/workspace/[id]/page.tsx
@@ -3,12 +3,21 @@ import CallAPI from "@/ultils/workspace-call-api";
 
 type Props = {params: {id: string}};
 
+async function Get(id: string) {
+	const response = await fetch(
+		// `${process.env.HOST_URL || ""}/api/workspace?id=${id}`,
+		`${process.env.HOST_URL || ""}/api/workspace?id=${id}`,
+		{cache: "no-cache"}
+	);
+	return await response.json();
+}
 export default async function WorkspaceItemPage({params}: Props) {
-	const item = await CallAPI.getItem(params.id);
+	const item = await Get(params.id);
 	return (
 		<div>
+			<pre>{JSON.stringify(`${process.env.HOST_URL || ""}`, null, 3)}</pre>
 			<pre>{JSON.stringify(item, null, 3)}</pre>
-			<div>WorkspaceItemPage</div>
+			<div className="text-xl font-bold">WorkspaceItemPage</div>
 		</div>
 	);
 }
