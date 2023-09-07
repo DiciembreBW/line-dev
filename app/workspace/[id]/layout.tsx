b65da6ba1ac3e70fea5412776c14fda759c9a2b6
mac <mac@macs-Mac-mini.local>
2023-09-07 18:20:22 +0700

    1

--- app/workspace/[id]/layout.tsx
+++ app/workspace/[id]/layout.tsx
@@ -5,21 +5,21 @@ import React from "react";
 
 type Props = {children: React.ReactNode; params: {id: string}};
 
-async function get(id: string) {
-	const res = await fetch(`${process.env.HOST_URL!}/api/workspace?id=${id}`);
-	return await res.json();
-}
+// async function get(id: string) {
+// 	const res = await fetch(`${process.env.HOST_URL!}/api/workspace?id=${id}`);
+// 	return await res.json();
+// }
 
 export default async function Layout({children, params}: Props) {
-	const id = await get(params.id);
+	// const id = await get(params.id);
 
 	const init_value: AppType = {
-		id: "",
+		id: params.id,
 		counter: 0,
 	};
 	return (
 		<div className="">
-			<pre>{JSON.stringify(id)}</pre>
+			{/* <pre>{JSON.stringify(id)}</pre> */}
 			<AppProvider value={init_value}>{children}</AppProvider>
 		</div>
 	);
