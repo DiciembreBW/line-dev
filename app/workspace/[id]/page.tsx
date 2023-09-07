4cbbbfd8db3890468f5318c797b2273281cfb471
mac <mac@macs-Mac-mini.local>
2023-09-07 21:51:30 +0700

    fix-4

--- app/workspace/[id]/page.tsx
+++ app/workspace/[id]/page.tsx
@@ -7,8 +7,8 @@ async function Get(id: string) {
 	// const URL = `${process.env.HOST_URL || ""}/api/workspace?id=${id}`;
 	// const URL = `${process.env.HOST_URL}`;
 	const response = await fetch(
-		// `${process.env.HOST_URL || ""}/api/workspace?id=${id}`,
-		`https://line-dev-smoky.vercel.app/api/workspace?id=${id}`,
+		`${process.env.HOST_URL || ""}/api/workspace?id=${id}`,
+		// `https://line-dev-smoky.vercel.app/api/workspace?id=${id}`,
 		{cache: "no-cache"}
 	);
 	return await response.json();
@@ -20,7 +20,7 @@ export default async function WorkspaceItemPage({params}: Props) {
 	return (
 		<div>
 			{/* <pre>{JSON.stringify(`${process.env.HOST_URL || ""}`, null, 3)}</pre> */}
-			<pre>{JSON.stringify(item, null, 3)}</pre>
+			<pre>{JSON.stringify(item, null, 3)}</pre> adksakdjadj
 			<div className="text-xl font-bold">WorkspaceItemPage</div>
 		</div>
 	);
