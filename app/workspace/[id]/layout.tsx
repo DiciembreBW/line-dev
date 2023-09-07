67d5610a5d5ff3626cc381eefbc4c3e9f728219a
mac <mac@macs-Mac-mini.local>
2023-09-07 21:16:23 +0700

    fix-4

--- app/workspace/[id]/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-type Props = {children: React.ReactNode; params: {id: string}};
-
-export default async function Layout({children, params}: Props) {
-	return <div className="">{children}</div>;
-}
