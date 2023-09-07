b7a46c23f40e06f3d62fb22b84f590a8f591702b
mac <mac@macs-Mac-mini.local>
2023-09-07 21:24:39 +0700

    fix-4

--- app/workspace/[id]/layout.tsx
+++ app/workspace/[id]/layout.tsx
@@ -1,7 +1,14 @@
+import CallAPI from "@/ultils/workspace-call-api";
 import React from "react";
 
 type Props = {params: {id: string}};
 
-export default async function WorkspaceItemPageLayout({}: Props) {
-	return <div>WorkspaceItemPageLayout</div>;
+export default async function WorkspaceItemPageLayout({params}: Props) {
+	const item = await CallAPI.getItem(params.id);
+	return (
+		<div>
+			<pre>{JSON.stringify(item)}</pre>
+			<div>WorkspaceItemPageLayout</div>
+		</div>
+	);
 }
