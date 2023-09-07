a14375f8e42849b37a2a45472166cb874ffee28f
mac <mac@macs-Mac-mini.local>
2023-09-07 18:17:15 +0700

    add Hosting url Production Mode

--- app/workspace/[id]/layout.tsx
+++ app/workspace/[id]/layout.tsx
@@ -1,11 +1,12 @@
 import AppProvider from "@/context/app/AppProvider";
 import {AppType} from "@/context/app/type";
+import Host from "@/libs/utilities/Host";
 import React from "react";
 
 type Props = {children: React.ReactNode; params: {id: string}};
 
 async function get(id: string) {
-	const res = await fetch(`http://localhost:3000/api/workspace?id=${id}`);
+	const res = await fetch(`${process.env.HOST_URL!}/api/workspace?id=${id}`);
 	return await res.json();
 }
 
@@ -13,11 +14,12 @@ export default async function Layout({children, params}: Props) {
 	const id = await get(params.id);
 
 	const init_value: AppType = {
-		id: id.value,
+		id: "",
 		counter: 0,
 	};
 	return (
 		<div className="">
+			<pre>{JSON.stringify(id)}</pre>
 			<AppProvider value={init_value}>{children}</AppProvider>
 		</div>
 	);
