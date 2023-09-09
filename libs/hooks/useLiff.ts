52439dc1ccc99afe375414fbddd728fcac1cd696
mac <mac@macs-Mac-mini.local>
2023-09-09 22:19:16 +0700

    line lif-2

--- libs/hooks/useLiff.ts
+++ libs/hooks/useLiff.ts
@@ -13,20 +13,32 @@ export default function useLiff({liffId}: Props) {
 	const [user, setUser] = useState<UserProfile>();
 
 	useEffect(() => {
-		liff
-			.init({liffId, withLoginOnExternalBrowser: true})
-			.then(async () => {
-				const user = liff.getDecodedIDToken();
+		// liff
+		// 	.init({liffId, withLoginOnExternalBrowser: true})
+		// 	.then(async () => {
+		// 		const user = liff.getDecodedIDToken();
 
-				setUser({
-					id: user?.sub,
-					name: user?.name,
-					picture: user?.picture,
-				});
-			})
-			.catch((err) => {
-				throw new Error(err);
+		// 		setUser({
+		// 			id: user?.sub,
+		// 			name: user?.name,
+		// 			picture: user?.picture,
+		// 		});
+		// 	})
+		// 	.catch((err) => {
+		// 		throw new Error(err);
+		// 	});
+
+		liff.ready.then(async () => {
+			const user = liff.getDecodedIDToken();
+
+			setUser({
+				id: user?.sub,
+				name: user?.name,
+				picture: user?.picture,
 			});
+		});
+
+		liff.init({liffId, withLoginOnExternalBrowser: true});
 	}, []);
 
 	return {user, liff};
