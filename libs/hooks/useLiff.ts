149b85e07e2fade5510c3ef41536b66422adaa30
mac <mac@macs-Mac-mini.local>
2023-09-10 22:27:09 +0700

    fix-1

--- libs/hooks/useLiff.ts
+++ libs/hooks/useLiff.ts
@@ -13,14 +13,16 @@ export default function useLiff({liffId}: Props) {
 	const [user, setUser] = useState<UserProfile>();
 
 	useEffect(() => {
-		liff.init({liffId, withLoginOnExternalBrowser: true}).then(() => {
+		liff.init({liffId}).then(() => {
 			liff.ready.then(async () => {
-				const user = liff.getDecodedIDToken();
-				setUser({
-					id: user?.sub,
-					name: user?.name,
-					picture: user?.picture,
-				});
+				if (liff.isInClient()) {
+					const user = liff.getDecodedIDToken();
+					setUser({
+						id: user?.sub,
+						name: user?.name,
+						picture: user?.picture,
+					});
+				}
 			});
 		});
 	}, []);
