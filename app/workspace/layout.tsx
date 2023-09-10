1e08e85b9a9801fc15ae827d0dc97712ac4678b5
mac <mac@macs-Mac-mini.local>
2023-09-10 22:36:40 +0700

    no loading user profile

--- app/workspace/layout.tsx
+++ app/workspace/layout.tsx
@@ -8,12 +8,12 @@ type Props = {children: React.ReactNode};
 export default function Layout({children}: Props) {
 	const {user} = useLiff({liffId: "2000394306-EVnwMxlm"});
 
-	if (user == undefined)
-		return (
-			<div className="h-screen grid justify-items-center items-center touch-none">
-				<CircularProgress className="text-zinc-400" color="inherit" size={100} />
-			</div>
-		);
+	// if (user == undefined)
+	// 	return (
+	// 		<div className="h-screen grid justify-items-center items-center touch-none">
+	// 			<CircularProgress className="text-zinc-400" color="inherit" size={100} />
+	// 		</div>
+	// 	);
 
 	return <GlobalProvider init={{user}}>{children}</GlobalProvider>;
 }
