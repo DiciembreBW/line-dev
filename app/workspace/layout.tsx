0d22af998dce937e097acafdd7bd4740bbc07f0b
mac <mac@macs-Mac-mini.local>
2023-09-11 19:44:33 +0700

    re

--- app/workspace/layout.tsx
+++ app/workspace/layout.tsx
@@ -8,17 +8,16 @@ type Props = {children: React.ReactNode};
 export default function Layout({children}: Props) {
 	const {user, loading} = useLiff({liffId: "2000394306-EVnwMxlm"});
 
-	// console.log(user);
-	// console.log(loading);
+	// if (loading) {
+	// 	return (
+	// 		<div className="h-screen grid justify-items-center items-center touch-none">
+	// 			{/* <CircularProgress className="text-zinc-400" color="inherit" size={100} /> */}
+	// 			loading...
+	// 		</div>
+	// 	);
+	// } else {
+	// 	return <GlobalProvider init={{user}}>{children}</GlobalProvider>;
+	// }
 
-	if (loading) {
-		return (
-			<div className="h-screen grid justify-items-center items-center touch-none">
-				{/* <CircularProgress className="text-zinc-400" color="inherit" size={100} /> */}
-				loading...
-			</div>
-		);
-	} else {
-		return <GlobalProvider init={{user}}>{children}</GlobalProvider>;
-	}
+	return <GlobalProvider init={{user}}>{children}</GlobalProvider>;
 }
