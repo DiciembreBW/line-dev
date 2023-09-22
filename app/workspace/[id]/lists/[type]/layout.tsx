c1da32170cd01e912dd9f3dba850d5fbdb48e23e
mac <mac@macs-Mac-mini.local>
2023-09-22 19:33:44 +0700

    fix 7

--- app/workspace/[id]/lists/[type]/layout.tsx
+++ app/workspace/[id]/lists/[type]/layout.tsx
@@ -1,17 +1,28 @@
-// import Link from "next/link";
+// // import Link from "next/link";
+// import React from "react";
+
+// type Props = {children: React.ReactNode};
+
+// export default function Layout({children}: Props) {
+// 	return (
+// 		<div className="bg-zinc-200/50 ">
+// 			{/* <div className="grid justify-between px-3 py-2">
+// 				<Link href={"/workspace/lists"} className="border rounded-lg px-3 py-2">
+// 					Lists
+// 				</Link>
+// 			</div> */}
+// 			{children}
+// 		</div>
+// 	);
+// }
+
 import React from "react";
 
 type Props = {children: React.ReactNode};
 
-export default function Layout({children}: Props) {
+export default function layout({children}: Props) {
 	return (
-		<div className="bg-zinc-200/50 ">
-			{/* <div className="grid justify-between px-3 py-2">
-				<Link href={"/workspace/lists"} className="border rounded-lg px-3 py-2">
-					Lists
-				</Link>
-			</div> */}
-			{children}
-		</div>
+		// <div>layout</div>
+		<div>{children}</div>
 	);
 }
