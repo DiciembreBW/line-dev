9ba5108e07bd387940d3371ad23f3ea8b9cc1613
mac <mac@macs-Mac-mini.local>
2023-09-22 19:42:00 +0700

    fix 7

--- app/workspace/[id]/lists/[type]/layout.tsx
+++ app/workspace/[id]/lists/[type]/layout.tsx
@@ -1,28 +1,7 @@
-// // import Link from "next/link";
-// import React from "react";
-
-// type Props = {children: React.ReactNode};
-
-// export default function Layout({children}: Props) {
-// 	return (
-// 		<div className="bg-zinc-200/50 ">
-// 			{/* <div className="grid justify-between px-3 py-2">
-// 				<Link href={"/workspace/lists"} className="border rounded-lg px-3 py-2">
-// 					Lists
-// 				</Link>
-// 			</div> */}
-// 			{children}
-// 		</div>
-// 	);
-// }
-
 import React from "react";
 
 type Props = {children: React.ReactNode};
 
 export default function layout({children}: Props) {
-	return (
-		// <div>layout</div>
-		<div>{children}</div>
-	);
+	return <div className="bg-zinc-200/50 ">{children}</div>;
 }
--- app/workspace/[id]/page.tsx
+++ app/workspace/[id]/page.tsx
@@ -1,37 +1,37 @@
-// import ItemNav from "@/components/app/Navbar/ItemNav";
-
-// import React from "react";
-
-// type Props = {};
-
-// export default function WorkspaceItemPage({}: Props) {
-// 	return (
-// 		<>
-// 			{/* <WorkspaceNav /> */}
-// 			{/* <pre>{JSON.stringify(app, null, 3)}</pre> */}
-
-// 			<ItemNav />
-// 			<div className="px-3 py-2 grid gap-2 content-center  ">
-// 				<div className="flex snap-x snap-mandatory snap-always gap-2 overflow-x-scroll">
-// 					{/* <ShortDesign name="เสื้อ t-shirt" neck="คอกลม" sleeve="แขนสั้น" />
-// 					<LongDesign name="เสื้อ t-shirt แขนยาว" neck="คอกลม" sleeve="แขนยาว" />
-// 					<ShortDesign name="เสื้อ โปโล" neck="คอโปโล" sleeve="แขนสั้น" /> */}
-// 					3D
-// 				</div>
-// 			</div>
-// 		</>
-// 	);
-// }
+import ItemNav from "@/components/app/Navbar/ItemNav";
 
 import React from "react";
 
-type Props = {params: {id: string}};
+type Props = {};
 
-export default function page({params}: Props) {
+export default function WorkspaceItemPage({}: Props) {
 	return (
-		<div>
-			id page
-			<pre>{JSON.stringify(params, null, 3)}</pre>
-		</div>
+		<>
+			{/* <WorkspaceNav /> */}
+			{/* <pre>{JSON.stringify(app, null, 3)}</pre> */}
+
+			<ItemNav />
+			<div className="px-3 py-2 grid gap-2 content-center  ">
+				<div className="flex snap-x snap-mandatory snap-always gap-2 overflow-x-scroll">
+					{/* <ShortDesign name="เสื้อ t-shirt" neck="คอกลม" sleeve="แขนสั้น" />
+					<LongDesign name="เสื้อ t-shirt แขนยาว" neck="คอกลม" sleeve="แขนยาว" />
+					<ShortDesign name="เสื้อ โปโล" neck="คอโปโล" sleeve="แขนสั้น" /> */}
+					#3d
+				</div>
+			</div>
+		</>
 	);
 }
+
+// import React from "react";
+
+// type Props = {params: {id: string}};
+
+// export default function page({params}: Props) {
+// 	return (
+// 		<div>
+// 			id page
+// 			<pre>{JSON.stringify(params, null, 3)}</pre>
+// 		</div>
+// 	);
+// }
