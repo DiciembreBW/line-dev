d01da174c492cfae7a503b27e721cdfe2b2ac0c1
mac <mac@macs-Mac-mini.local>
2023-09-22 19:48:54 +0700

    fix 7

--- app/workspace/[id]/layout.tsx
+++ app/workspace/[id]/layout.tsx
@@ -1,49 +1,49 @@
-// import AppProvider from "@/context/app/AppProvider";
-// import CallAPI from "@/ultils/workspace-call-api";
-// import React from "react";
-
-// type Props = {children: React.ReactNode; params: {id: string}};
-
-// export default async function WorkspaceItemPageLayout({
-// 	children,
-// 	params,
-// }: Props) {
-// 	const item = await CallAPI.getItem(params.id);
-
-// 	// return <>{params.id}</>;
-// 	if (params.id == "create")
-// 		return (
-// 			<>
-// 				<AppProvider
-// 					value={{
-// 						name: "",
-// 						counter: 0,
-// 						status: 0,
-// 						id: "create",
-// 						init: false,
-// 						address: "",
-// 						items: [],
-// 					}}>
-// 					{children}
-// 				</AppProvider>
-// 			</>
-// 		);
-
-// 	if (item == null) return <>no value</>;
-
-// 	return (
-// 		<>
-// 			{/* console.log(params.id); */}
-// 			{/* {params.id} */}
-// 			<AppProvider value={item}>{children}</AppProvider>
-// 		</>
-// 	);
-// }
-
+import AppProvider from "@/context/app/AppProvider";
+import CallAPI from "@/ultils/workspace-call-api";
 import React from "react";
 
-type Props = {children: React.ReactNode};
+type Props = {children: React.ReactNode; params: {id: string}};
 
-export default function Layout({children}: Props) {
-	return <div>{children}</div>;
+export default async function WorkspaceItemPageLayout({
+	children,
+	params,
+}: Props) {
+	const item = await CallAPI.getItem(params.id);
+
+	// return <>{params.id}</>;
+	if (params.id == "create")
+		return (
+			<>
+				<AppProvider
+					value={{
+						name: "",
+						counter: 0,
+						status: 0,
+						id: "create",
+						init: false,
+						address: "",
+						items: [],
+					}}>
+					{children}
+				</AppProvider>
+			</>
+		);
+
+	if (item == null) return <>no value</>;
+
+	return (
+		<>
+			{/* console.log(params.id); */}
+			{/* {params.id} */}
+			<AppProvider value={item}>{children}</AppProvider>
+		</>
+	);
 }
+
+// import React from "react";
+
+// type Props = {children: React.ReactNode};
+
+// export default function Layout({children}: Props) {
+// 	return <div>{children}</div>;
+// }
--- app/workspace/[id]/lists/page.tsx
+++ app/workspace/[id]/lists/page.tsx
@@ -1,35 +1,35 @@
-// "use client";
-// import ItemNav from "@/components/app/Navbar/ItemNav";
-// import React from "react";
-// import ListItem from "@/components/app/ui/lists/home/ListItem";
-
-// type Props = {};
-
-// export default function page({}: Props) {
-// 	return (
-// 		<>
-// 			{/* <AppNav /> */}
-
-// 			<ItemNav />
-
-// 			<div className="px-3 py-2">
-// 				<div className="grid gap-2">
-// 					<ListItem />
-// 				</div>
-// 			</div>
-// 		</>
-// 	);
-// }
-
+"use client";
+import ItemNav from "@/components/app/Navbar/ItemNav";
 import React from "react";
+import ListItem from "@/components/app/ui/lists/home/ListItem";
 
-type Props = {params: {type: string}};
+type Props = {};
 
-export default function ListPage({params}: Props) {
+export default function page({}: Props) {
 	return (
-		<div>
-			ListPage
-			<pre>{JSON.stringify(params)}</pre>
-		</div>
+		<>
+			{/* <AppNav /> */}
+
+			<ItemNav />
+
+			<div className="px-3 py-2">
+				<div className="grid gap-2">
+					<ListItem />
+				</div>
+			</div>
+		</>
 	);
 }
+
+// import React from "react";
+
+// type Props = {params: {type: string}};
+
+// export default function ListPage({params}: Props) {
+// 	return (
+// 		<div>
+// 			ListPage
+// 			<pre>{JSON.stringify(params)}</pre>
+// 		</div>
+// 	);
+// }
