cfc3ec5a075cae32a8e0b1501f155253a5597d4d
mac <mac@macs-Mac-mini.local>
2023-09-22 12:53:57 +0700

    fix6

--- app/workspace/[id]/layout.tsx
+++ app/workspace/[id]/layout.tsx
@@ -1,41 +1,49 @@
-import AppProvider from "@/context/app/AppProvider";
-import CallAPI from "@/ultils/workspace-call-api";
+// import AppProvider from "@/context/app/AppProvider";
+// import CallAPI from "@/ultils/workspace-call-api";
+// import React from "react";
+
+// type Props = {children: React.ReactNode; params: {id: string}};
+
+// export default async function WorkspaceItemPageLayout({
+// 	children,
+// 	params,
+// }: Props) {
+// 	const item = await CallAPI.getItem(params.id);
+
+// 	// return <>{params.id}</>;
+// 	if (params.id == "create")
+// 		return (
+// 			<>
+// 				<AppProvider
+// 					value={{
+// 						name: "",
+// 						counter: 0,
+// 						status: 0,
+// 						id: "create",
+// 						init: false,
+// 						address: "",
+// 						items: [],
+// 					}}>
+// 					{children}
+// 				</AppProvider>
+// 			</>
+// 		);
+
+// 	if (item == null) return <>no value</>;
+
+// 	return (
+// 		<>
+// 			{/* console.log(params.id); */}
+// 			{/* {params.id} */}
+// 			<AppProvider value={item}>{children}</AppProvider>
+// 		</>
+// 	);
+// }
+
 import React from "react";
 
-type Props = {children: React.ReactNode; params: {id: string}};
+type Props = {children: React.ReactNode};
 
-export default async function WorkspaceItemPageLayout({
-	children,
-	params,
-}: Props) {
-	const item = await CallAPI.getItem(params.id);
-
-	// return <>{params.id}</>;
-	if (params.id == "create")
-		return (
-			<>
-				<AppProvider
-					value={{
-						name: "",
-						counter: 0,
-						status: 0,
-						id: "create",
-						init: false,
-						address: "",
-						items: [],
-					}}>
-					{children}
-				</AppProvider>
-			</>
-		);
-
-	if (item == null) return <>no value</>;
-
-	return (
-		<>
-			{/* console.log(params.id); */}
-			{/* {params.id} */}
-			<AppProvider value={item}>{children}</AppProvider>
-		</>
-	);
+export default function layout({children}: Props) {
+	return <div>{children}</div>;
 }
